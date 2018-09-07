5b048d51759b0e516b0f14d5c14735cbc3e1638c
Robin.Han <hbinboy@gmail.com>
2018-09-07 17:31:28 +0800

    Add read function that get message with header.

--- src/com/hb/pocket/serverv2/thread/ServerSelectorReadTask.java
+++ src/com/hb/pocket/serverv2/thread/ServerSelectorReadTask.java
@@ -1,6 +1,8 @@
 package com.hb.pocket.serverv2.thread;
 
+import com.hb.pocket.data.DataManager;
 import com.hb.pocket.serverv2.thread.callback.IServerSelectorReadCallback;
+import com.hb.utils.config.ServerConfig;
 import com.hb.utils.log.MyLog;
 
 import java.io.IOException;
@@ -46,7 +48,22 @@ public class ServerSelectorReadTask implements Runnable {
      * @return
      * @throws IOException
      */
-    private static int read(SocketChannel channel,IServerSelectorReadCallback iServerSelectorReadCallback) throws IOException {
+    private int read(SocketChannel channel,IServerSelectorReadCallback iServerSelectorReadCallback) throws IOException {
+        if (ServerConfig.readDataWithHeader) {
+            return readDataWithHeader(channel, iServerSelectorReadCallback);
+        } else {
+            return readRawData(channel, iServerSelectorReadCallback);
+        }
+    }
+
+    /**
+     * Process the read message without Header {@link com.hb.pocket.data.header.Header}
+     * @param channel
+     * @param iServerSelectorReadCallback
+     * @return
+     * @throws IOException
+     */
+    private int readRawData(SocketChannel channel, IServerSelectorReadCallback iServerSelectorReadCallback) throws IOException {
         try {
             ByteBuffer buffer = ByteBuffer.allocate(1024); // Alloc heapByteBuffer
             int len = channel.read(buffer); // Unitil no data or the buffer is full.
@@ -60,6 +77,54 @@ public class ServerSelectorReadTask implements Runnable {
             }
             return len;
         } catch (IOException e) {
+            if (iServerSelectorReadCallback != null) {
+                iServerSelectorReadCallback.onEndRead(null, -1);
+            }
+            return -1;
+        }
+    }
+
+    /**
+     * Process the read message with Header {@link com.hb.pocket.data.header.Header}
+     * @param channel
+     * @param iServerSelectorReadCallback
+     * @return
+     * @throws IOException
+     */
+    private int readDataWithHeader(SocketChannel channel, IServerSelectorReadCallback iServerSelectorReadCallback) throws IOException {
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        byte[] bytes = buffer.array();
+        try {
+            int len = channel.read(buffer);
+            int remainLen = len;
+            int bodyLen = 0;
+            int offset = 0;
+            buffer.position(0);
+            while (remainLen > 0) {
+                byte[] bufferBak = new byte[len - offset];
+                System.arraycopy(bytes, offset, bufferBak, 0, len - offset);
+                MyLog.d(TAG, "" + len);
+                DataManager dataManager = new DataManager();
+                if (len > 0) {
+                    if (dataManager.getReceiveDataPackageData(bufferBak) != null) {
+                        dataManager.getBody().getData();
+                        offset += dataManager.getHeader().getHeadLen() + dataManager.getHeader().getDataLen();
+
+                        remainLen = len - offset;
+                        MyLog.i(TAG, dataManager.getBody().getData()); // buffer.array()：get the HeapByteFuffer raw data.
+                    }
+                }
+                if (iServerSelectorReadCallback != null && len > 0) {
+                    iServerSelectorReadCallback.onEndRead(dataManager.getBody().getData(), dataManager.getBody().getData().length());
+                } else {
+                    iServerSelectorReadCallback.onEndRead(null, bodyLen);
+                }
+            }
+            return len;
+        } catch (IOException e) {
+            if (channel != null) {
+                channel.close();
+            }
             if (iServerSelectorReadCallback != null) {
                 iServerSelectorReadCallback.onEndRead(null, -1);
             }
